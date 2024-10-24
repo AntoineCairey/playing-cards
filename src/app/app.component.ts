05296d7d575a889f0a78bd639cf457baff31661c
Antoine Cairey <cairey.antoine@gmail.com>
2024-10-24 17:34:01 +0200

    add login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router, RouterOutlet } from '@angular/router';
+import { LoginService } from './services/login/login.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnDestroy {
+  private router = inject(Router);
+  loginService = inject(LoginService);
+
+  private logoutSubscription: Subscription | null = null;
+
+  logout() {
+    this.logoutSubscription = this.loginService.logout().subscribe({
+      next: (_) => {
+        this.navigateToLogin();
+      },
+      error: (_) => {
+        this.navigateToLogin();
+      },
+    });
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['login']);
+  }
+
+  navigateHome() {
+    this.router.navigate(['home']);
+  }
+
+  ngOnDestroy(): void {
+    this.logoutSubscription?.unsubscribe();
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { MonsterListComponent } from './pages/monster-list/monster-list.component';
 import { MonsterComponent } from './pages/monster/monster.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LoginComponent } from './pages/login/login.component';
+import { isLoggedInGuard } from './guards/is-logged-in.guard';
 
 export const routes: Routes = [
   {
@@ -12,6 +14,11 @@ export const routes: Routes = [
   {
     path: 'home',
     component: MonsterListComponent,
+    canActivate: [isLoggedInGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: 'monster',
@@ -19,10 +26,12 @@ export const routes: Routes = [
       {
         path: '',
         component: MonsterComponent,
+        canActivate: [isLoggedInGuard],
       },
       {
         path: ':id',
         component: MonsterComponent,
+        canActivate: [isLoggedInGuard],
       },
     ],
   },
